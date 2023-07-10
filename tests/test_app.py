cacf5b4d1943172d2cb65d58342ebb2d0efbf958
Izaac Molina <39788504+IzMo2000@users.noreply.github.com>
2023-07-10 16:34:16 -0700

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -17,7 +17,7 @@ class AppTests(unittest.TestCase):
         response = self.app.get('/', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
-    def test_result_page(self);
+    def test_result_page(self):
         response = self.app.get('/result', follow_redirects=True)
         self.assertEqual(response.status_code, 200)
 
