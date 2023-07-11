961e24f5672153be28b396cf61de3cf0e928e4f1
Andrew Espinosa <adespinosa2002@gmail.com>
2023-07-11 05:57:51 +0000

    Added Layout + Home Page

--- app.py
+++ app.py
@@ -5,9 +5,10 @@ import git
 app = Flask(__name__)
 
 # define home page
-@app.route("/")                          
+@app.route("/")                         
+@app.route("/home")                           
 def hello_world():
-    return "<p>Hello, World!!!</p>" 
+    return render_template('home.html') 
 
 # define route to update_server, connecting git repo to PythonAnywhere
 @app.route("/update_server", methods=['POST'])
@@ -22,4 +23,4 @@ def webhook():
 
 # start server
 if __name__ == '__main__':               
-    app.run(debug=True, host="0.0.0.0")       
+    app.run(debug=True, host="0.0.0.0")        
